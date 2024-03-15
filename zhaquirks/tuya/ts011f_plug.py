5c5f5eaa2ae712b860ae2fd576a50d265fbabe87
Daniel DA <daniel-da@users.noreply.github.com>
2024-03-15 20:16:59 +0100

    TS011F _TZ3000_okaz9tjs update

--- zhaquirks/tuya/ts011f_plug.py
+++ zhaquirks/tuya/ts011f_plug.py
@@ -1046,16 +1046,13 @@ class Plug_4AC_2USB_Metering(EnchantedDevice):
         },
     }
 
-
 class Plug_v2(EnchantedDevice):
     """Another TS011F Tuya plug. First one using this definition is _TZ3000_okaz9tjs."""
 
     quirk_id = TUYA_PLUG_ONOFF
 
     signature = {
-        MODELS_INFO: [
-            ("_TZ3000_okaz9tjs", "TS011F")
-        ],
+        MODEL: "TS011F",
         ENDPOINTS: {
             # "profile_id": 260,
             # "device_type": "0x0051",
@@ -1091,6 +1088,63 @@ class Plug_v2(EnchantedDevice):
                     TuyaZBOnOffAttributeCluster,
                     TuyaZBMeteringClusterWithUnit,
                     TuyaZBElectricalMeasurement,
+                    TuyaZBExternalSwitchTypeCluster,
+                ],
+                OUTPUT_CLUSTERS: [],
+            },
+        },
+    }
+
+class Plug_v2b(EnchantedDevice):
+    """Another TS011F Tuya plug. First one using this definition is _TZ3000_okaz9tjs."""
+
+    quirk_id = TUYA_PLUG_ONOFF
+
+    signature = {
+        MODELS_INFO: [
+            ("_TZ3000_okaz9tjs", "TS011F")
+        ],
+        ENDPOINTS: {
+            # "profile_id": 260,
+            # "device_type": "0x0100",
+            # "in_clusters": [ "0x0000","0x0003","0x0004","0x0005","0x0006","0x000a","0x0702","0x0b04","0x1000","0xe000","0xe001"],
+            # "out_clusters": []
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    Time.cluster_id,
+                    TuyaZBMeteringClusterWithUnit.cluster_id,
+                    TuyaZBElectricalMeasurement.cluster_id,
+                    LightLink.cluster_id,
+                    TuyaZBE000Cluster.cluster_id,
+                    TuyaZBExternalSwitchTypeCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [],
+            },
+        },
+    }
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    TuyaZBOnOffAttributeCluster,
+                    Time.cluster_id,
+                    TuyaZBMeteringClusterWithUnit,
+                    TuyaZBElectricalMeasurement,
+                    LightLink.cluster_id,
+                    TuyaZBE000Cluster.cluster_id,
                     TuyaZBExternalSwitchTypeCluster,
                 ],
                 OUTPUT_CLUSTERS: [],
